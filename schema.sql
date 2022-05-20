3de09fcc22380227807e88a0f5a7fd00abb10a0f
Abe <abeschnake@gmail.com>
2022-05-20 12:55:54 -0400

    added updated files

--- schema.sql
+++ schema.sql
@@ -16,7 +16,6 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
 
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
@@ -25,7 +24,7 @@ dept_no VARCHAR(4) NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+    PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE salaries (
@@ -38,19 +37,38 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
+	index INT NOT NULL,
 	emp_no INT NOT NULL,
 	dept_no VARCHAR (4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (index)
 );
 
 CREATE TABLE titles (
+	index INT NOT NULL,
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	PRIMARY KEY (emp_no),
+	PRIMARY KEY (index),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
+
+SELECT * FROM salaries;
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
